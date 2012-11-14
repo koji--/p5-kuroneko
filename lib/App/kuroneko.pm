bbf1120d9cc16666070c2a44dc72d42b6413b104
koji_magi <kojiel@gmail.com>
2012-11-15 02:17:27 +0900

    commit skelton files

---
+++ lib/App/kuroneko.pm
@@ -0,0 +1,32 @@
+package App::kuroneko;
+use strict;
+use warnings;
+our $VERSION = '0.001';
+
+1;
+__END__
+
+=head1 NAME
+
+App::kuroneko -
+
+=head1 SYNOPSIS
+
+  use App::kuroneko;
+
+=head1 DESCRIPTION
+
+App::kuroneko is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
